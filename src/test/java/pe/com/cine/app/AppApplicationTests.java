20a5a35caa53466069ef6ee4319f19d409881ee8
72422998 <81695790+72422998@users.noreply.github.com>
2023-05-10 18:30:58 -0500

    chore: :tada: Project init

---
+++ src/test/java/pe/com/cine/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.cine.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
