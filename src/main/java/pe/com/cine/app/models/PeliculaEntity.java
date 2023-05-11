dfbe0b8d38a33818c4fb1efaaeefb56fa1795e27
Alex <72656185@certus.edu.pe>
2023-05-10 21:05:56 -0500

    feat: :building_construction: creacion de entidad
    He hecho una entidad llamada peliculas

---
+++ src/main/java/pe/com/cine/app/models/PeliculaEntity.java
@@ -0,0 +1,83 @@
+package pe.com.cine.app.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "peliculas")
+public class PeliculaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private String director;
+    private Integer anio_estreno;
+    private Integer duracion;
+    private String genero;
+    private String sinopsis;
+
+    public PeliculaEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public Integer getAnio_estreno() {
+        return anio_estreno;
+    }
+
+    public void setAnio_estreno(Integer anio_estreno) {
+        this.anio_estreno = anio_estreno;
+    }
+
+    public Integer getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(Integer duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getSinopsis() {
+        return sinopsis;
+    }
+
+    public void setSinopsis(String sinopsis) {
+        this.sinopsis = sinopsis;
+    }
+
+}
