15fb906e450ba713a808a82756a9ee6104bcc7d5
DavidChambilla <anvor127@gmail.com>
2023-05-10 21:29:48 -0500

    feat: :building_construction: create salasEntity
    create tables in salasEntity

---
+++ src/main/java/pe/com/cine/app/models/SalasEntity.java
@@ -0,0 +1,55 @@
+package pe.com.cine.app.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "salas")
+public class SalasEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String ubicacion;
+    private Integer capacidad;
+
+    public SalasEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public Integer getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(Integer capacidad) {
+        this.capacidad = capacidad;
+    }
+
+}
